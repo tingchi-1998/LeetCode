ce945c920002240d546ff12255ab53e7965fe5fb
angel871123 <angel871123@icloud.com>
2024-04-12 16:37:57 +0800

    Length of Last Word

---
+++ 58.length-of-last-word.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode id=58 lang=python
+#
+# [58] Length of Last Word
+#
+
+# @lc code=start
+class Solution(object):
+    def lengthOfLastWord(self, s):
+       return len(s.split()[-1])
+        
+# @lc code=end
+
