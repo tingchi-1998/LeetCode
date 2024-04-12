2657ce1a3d2556a5d080ab55d8281573d8d6fa7c
angel871123 <angel871123@icloud.com>
2024-04-12 15:41:21 +0800

    Search Insert Position

---
+++ 35.search-insert-position.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode id=35 lang=python
+#
+# [35] Search Insert Position
+#
+
+# @lc code=start
+class Solution(object):
+    def searchInsert(self, nums, target):
+        l = 0
+        r = len(nums) - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] < target:
+                l = mid + 1
+            elif nums[mid] > target:
+                r = mid - 1
+            else:
+                return mid
+        return l
+        
+# @lc code=end
+
